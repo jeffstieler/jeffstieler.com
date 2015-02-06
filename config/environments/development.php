a4d548495d90e149927f6a3401803e002feb0159
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-02-06 00:19:12 -0700

    Add memcached-redux.

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,3 +11,5 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+$memcached_servers = array('127.0.0.1:11211');
--- config/environments/production.php
+++ config/environments/production.php
@@ -11,3 +11,5 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+
+$memcached_servers = array('127.0.0.1:11211');
