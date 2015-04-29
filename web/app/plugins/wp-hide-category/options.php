b532838264d6311fc4c53530e3a31a3980f73fff
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-04-28 19:14:46 -0600

    Add wp-hide-category plugin files (for modification).

---
+++ web/app/plugins/wp-hide-category/options.php
@@ -0,0 +1,73 @@
+<?php
+function wp_hide_category_register_settings() {
+	add_option('wp_hide_category_hide_place', array("front_page"));
+	add_option('wp_hide_category_id', '');
+	register_setting('wp_hide_category_options', 'wp_hide_category_hide_place');
+	register_setting('wp_hide_category_options', 'wp_hide_category_id');
+}
+add_action('admin_init', 'wp_hide_category_register_settings');
+
+function wp_hide_category_register_options_page() {
+	add_options_page(__('WP Hide Category Options Page', WP_HIDE_CATEGORY_TEXT_DOMAIN), __('WP Hide Category', WP_HIDE_CATEGORY_TEXT_DOMAIN), 'manage_options', WP_HIDE_CATEGORY_TEXT_DOMAIN.'-options', 'wp_hide_category_options_page');
+}
+add_action('admin_menu', 'wp_hide_category_register_options_page');
+
+function wp_hide_category_get_checked($checkbox_name, $check_value){
+	if(is_array($checkbox_name)){
+		if(in_array($check_value, $checkbox_name)){
+			?> checked="checked"<?php
+		}
+	}
+}
+
+function wp_hide_category_get_checkbox_option($checkbox_name, $checkbox_value, $checkbox_id){
+	for($num = 0; $num < count($checkbox_id); $num++){
+		$checkbox_value_each = $checkbox_value[$num];
+		$checkbox_id_each = $checkbox_id[$num];
+		?>
+		<input type="checkbox" name="<?php echo $checkbox_name; ?>[]" id="<?php echo $checkbox_id_each; ?>" value="<?php echo $checkbox_id_each; ?>"<?php wp_hide_category_get_checked(get_option($checkbox_name), $checkbox_id_each); ?>><label for="<?php echo $checkbox_id_each; ?>"><?php echo $checkbox_value_each; ?></label>
+	<?php
+	}
+}
+
+function cat_list_recursive($parentid = 0, $selected = array()){
+	$cats = get_categories('hide_empty=0&child_of='.$parentid.'&parent='.$parentid);
+	if(count($cats) > 0){
+		$str = '<ul class="children">';
+		foreach($cats as $c){
+			$sel = (isset($selected) && in_array($c->cat_ID, $selected)) ? true : false;
+			$str .= '<li><input type="checkbox" name="wp_hide_category_id[]" value="'.$c->cat_ID.'" id="catid_'.$c->cat_ID.'"'.(($sel)?' checked="checked"':'').' /> <label for="catid_'.$c->cat_ID.'">'.esc_html($c->cat_name).'</label>'.cat_list_recursive($c->cat_ID,$selected).'</li>';
+		}
+		$str .= '</ul>';
+		return $str;
+	}
+	return '';
+}
+
+function wp_hide_category_options_page() {
+?>
+<div class="wrap">
+	<h2><?php _e("WP Hide Category Options Page", WP_HIDE_CATEGORY_TEXT_DOMAIN); ?></h2>
+	<form method="post" action="options.php">
+		<?php settings_fields('wp_hide_category_options'); ?>
+		<h3><?php _e("General Options", WP_HIDE_CATEGORY_TEXT_DOMAIN); ?></h3>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row"><label for="wp_hide_category_hide_place"><?php _e("Where do you want to hide the category?", WP_HIDE_CATEGORY_TEXT_DOMAIN); ?></label></th>
+					<td>
+						<?php wp_hide_category_get_checkbox_option("wp_hide_category_hide_place", array(__('Home', WP_HIDE_CATEGORY_TEXT_DOMAIN), __('Post', WP_HIDE_CATEGORY_TEXT_DOMAIN), __('Page', WP_HIDE_CATEGORY_TEXT_DOMAIN), __('Archive', WP_HIDE_CATEGORY_TEXT_DOMAIN), __('Search Result', WP_HIDE_CATEGORY_TEXT_DOMAIN)), array('front_page', 'single', 'page', 'archive', 'search')); ?>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row"><label for="wp_hide_category_id"><?php _e("The Category ID that you want to hide: ", WP_HIDE_CATEGORY_TEXT_DOMAIN); ?></label></th>
+					<td>
+						<?php echo cat_list_recursive(0, get_option('wp_hide_category_id')) ?>
+					</td>
+				</tr>
+			</table>
+		<?php submit_button(); ?>
+	</form>
+</div>
+<?php
+}
+?>
---
+++ web/app/plugins/wp-hide-category/wp-hide-category.php
@@ -0,0 +1,71 @@
+<?php
+/*
+
+**************************************************************************
+
+Plugin Name:  WP Hide Category
+Plugin URI:   http://www.arefly.com/wordpress-index-hide-category/
+Description:  Hide specific category in your blog.
+Version:      1.0.7
+Author:       Arefly
+Author URI:   http://www.arefly.com/
+Text Domain:  wp-hide-category
+Domain Path:  /lang/
+
+**************************************************************************
+
+	Copyright 2014  Arefly  (email : eflyjason@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+**************************************************************************/
+
+define("WP_HIDE_CATEGORY_PLUGIN_URL", plugin_dir_url( __FILE__ ));
+define("WP_HIDE_CATEGORY_FULL_DIR", plugin_dir_path( __FILE__ ));
+define("WP_HIDE_CATEGORY_TEXT_DOMAIN", "wp-hide-category");
+
+/* Plugin Localize */
+function wp_hide_category_load_plugin_textdomain() {
+	load_plugin_textdomain(WP_HIDE_CATEGORY_TEXT_DOMAIN, false, dirname(plugin_basename( __FILE__ )).'/lang/');
+}
+add_action('plugins_loaded', 'wp_hide_category_load_plugin_textdomain');
+
+include_once WP_HIDE_CATEGORY_FULL_DIR."options.php";
+
+/* Add Links to Plugins Management Page */
+function wp_hide_category_action_links($links){
+	$links[] = '<a href="'.get_admin_url(null, 'options-general.php?page='.WP_HIDE_CATEGORY_TEXT_DOMAIN.'-options').'">'.__("Settings", WP_HIDE_CATEGORY_TEXT_DOMAIN).'</a>';
+	return $links;
+}
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'wp_hide_category_action_links');
+
+function wp_hide_category($query) {  
+	if(!is_admin() && $query->is_main_query()) {
+		$wp_hide_category_hide_place = get_option('wp_hide_category_hide_place');
+		$category_hide = explode(',', get_option('wp_hide_category_id'));
+		if(!empty( $wp_hide_category_hide_place)) {
+			foreach($wp_hide_category_hide_place as $template_name){
+				$call_name = 'is_' . $template_name;
+				if($query->$call_name()){
+					if(!empty($category_hide)) {
+						foreach($category_hide as $cat_id) {
+							$query->set('cat', '-' . $cat_id);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+add_action('pre_get_posts', 'wp_hide_category');
