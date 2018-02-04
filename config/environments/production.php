146af061d7c155d8d0b206b2a4ad86d650a00de2
Jeff Stieler <jeff.m.stieler@gmail.com>
2018-02-03 17:52:40 -0700

    Update bedrock to master @ 9361944

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,15 +1,9 @@
 <?php
-/* Production */
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
-
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
-
+/** Production */
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+/** Disable all file modifications including updates and update notifications */
+define('DISALLOW_FILE_MODS', true);
 
 $memcached_servers = array('127.0.0.1:11211');
