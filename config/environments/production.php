e338b4f4463271809864881f76bd5a10ac257a91
Jeff Stieler <jeff.m.stieler@gmail.com>
2021-01-17 11:00:30 -0700

    Allow auto updates.

--- config/environments/production.php
+++ config/environments/production.php
@@ -3,7 +3,5 @@
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-/** Disable all file modifications including updates and update notifications */
-define('DISALLOW_FILE_MODS', true);
 
 $memcached_servers = array('127.0.0.1:11211');
